025b3560d60efea34c19a84f44ed1ed2be9bc038
Antti Haapala <antti.haapala@anttipatterns.com>
2017-08-31 01:07:54 +0300

    the tet.sqlalchemy.simple approach; use ServiceViews.

--- {{cookiecutter.project}}/{{cookiecutter.package}}/models/__init__.py
+++ {{cookiecutter.project}}/{{cookiecutter.package}}/models/__init__.py
@@ -1,70 +1,14 @@
-from sqlalchemy import engine_from_config
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import configure_mappers
-import zope.sqlalchemy
+from pyramid.config import Configurator
 
-# import or define all models here to ensure they are attached to the
-# Base.metadata prior to any initialization routines
-from .example import MyModel  # flake8: noqa
-
-# run configure_mappers after defining all of the models to ensure
-# all relationships can be setup
-configure_mappers()
+# include the models first so that we can safely call configure_mappers
+from .meta import Base
+from .example import MyModel
 
 
-def get_engine(settings, prefix='sqlalchemy.'):
-    return engine_from_config(settings, prefix)
+def includeme(config: Configurator):
+    # include the simple sqlalchemy extension
+    config.include('tet.sqlalchemy.simple')
 
+    # set up sqlalchemy using defaults
+    config.setup_sqlalchemy()
 
-def get_session_factory(engine):
-    factory = sessionmaker()
-    factory.configure(bind=engine)
-    return factory
-
-
-def get_tm_session(session_factory, transaction_manager):
-    """
-    Get a ``sqlalchemy.orm.Session`` instance backed by a transaction.
-    This function will hook the session to the transaction manager which
-    will take care of committing any changes.
-    - When using pyramid_tm it will automatically be committed or aborted
-      depending on whether an exception is raised.
-    - When using scripts you should wrap the session in a manager yourself.
-      For example::
-          import transaction
-          engine = get_engine(settings)
-          session_factory = get_session_factory(engine)
-          with transaction.manager:
-              dbsession = get_tm_session(session_factory, transaction.manager)
-    """
-    dbsession = session_factory()
-    zope.sqlalchemy.register(
-        dbsession, transaction_manager=transaction_manager)
-    return dbsession
-
-
-def includeme(config):
-    """
-    Initialize the model for a Tet app.
-    Activate this setup using ``config.include('{{ cookiecutter.package }}.models')``.
-    """
-
-    settings = config.get_settings()
-    settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
-
-    # use pyramid_tm to hook the transaction lifecycle to the request
-    config.include('pyramid_tm')
-
-    # use pyramid_retry to retry a request when transient exceptions occur
-    config.include('pyramid_retry')
-
-    session_factory = get_session_factory(get_engine(settings))
-    config.registry['dbsession_factory'] = session_factory
-
-    # make request.dbsession available for use in Pyramid
-    config.add_request_method(
-        # r.tm is the transaction manager used by pyramid_tm
-        lambda r: get_tm_session(session_factory, r.tm),
-        'dbsession',
-        reify=True
-    )
--- {{cookiecutter.project}}/{{cookiecutter.package}}/views/example.py
+++ {{cookiecutter.project}}/{{cookiecutter.package}}/views/example.py
@@ -1,26 +1,35 @@
 from tet.response import Response
-from tet.view import view_config
+from tet.view import view_config, ServiceViews
+from tet.services import autowired
 
 from sqlalchemy.exc import DBAPIError
+from sqlalchemy.orm import Session
 
 from ..models import MyModel
 
+class ExampleViews(ServiceViews):
+    db_session = autowired(Session)
 
-@view_config(route_name='home', renderer='{{cookiecutter.package}}:templates/index.tk')
-def my_view(request):
-    try:
-        one = request.dbsession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type=str('text/plain'), status_int=500)
-    return {'one': one, 'project': '{{cookiecutter.project}}'}
+    @view_config(route_name='home',
+                 renderer='{{cookiecutter.package}}:templates/index.tk')
+    def my_view(self):
+        try:
+            one = (self.db_session.query(MyModel)
+                   .filter(MyModel.name == 'one').first())
+        except DBAPIError:
+            return Response(conn_err_msg,
+                            content_type=str('text/plain'),
+                            status_int=500)
+
+        return {'one': one, 'project': '{{cookiecutter.project}}'}
 
 conn_err_msg = """\
 Tet is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
 
-1.  You may need to run the "initialize_{{cookiecutter.project}}_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
+1.  You may need to initialize the database using the default
+    migration: "alembic upgrade head" to initialize your database
+    tables.
 
 2.  Your database server may not be running.  Check that the
     database server referred to by the "sqlalchemy.url" setting in
